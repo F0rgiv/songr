90960fa263ba42f6d4906bfd94557ca3e2fd4f9b
James Mansour <jamestmansour@outlook.com>
2021-04-06 15:39:41 -0700

    Able to now connect to the DB

--- src/main/java/com/jamesmansour/songr/Album.java
+++ src/main/java/com/jamesmansour/songr/Album.java
@@ -1,11 +1,21 @@
 package com.jamesmansour.songr;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     String title;
     String artist;
     int songCount;
     int length;
     String imgUrl;
+
+    public Album() {}
 
     public Album(String title, String artist, int songCount, int length, String imgUrl) {
         this.title = title;
@@ -54,5 +64,7 @@ public class Album {
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
     }
+
+
 }
 
