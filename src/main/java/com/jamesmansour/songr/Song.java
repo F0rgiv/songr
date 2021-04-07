10dbb1bcabbdc2deb08d4dd272e255527c8adea6
James Mansour <jamestmansour@outlook.com>
2021-04-07 11:44:27 -0700

    Adds getter and setters to Song

--- src/main/java/com/jamesmansour/songr/Song.java
+++ src/main/java/com/jamesmansour/songr/Song.java
@@ -12,6 +12,48 @@ public class Song {
     int trackNumber;
 
     @ManyToOne
-    @JoinColumn(name="album_id")
+    @JoinColumn(name = "album_id")
     Album album;
+
+    public Song() {
+    }
+
+    public Song(String title, int length, int trackNumber, Album album) {
+        this.title = title;
+        this.length = length;
+        this.trackNumber = trackNumber;
+        this.album = album;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
 }
